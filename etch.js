2e45ea51c39037f954b6eca5578c86c52b3c1f4c
William Norboe <william.norboe@gmail.com>
2023-06-21 09:13:27 -0700

    added hover effect to the squares

--- etch.js
+++ etch.js
@@ -1,14 +1,21 @@
 
 let grid = document.querySelector("#container");
 
-
+let squareID = 1;
 for(let i = 0; i < 16; ++i){
     const row = document.createElement('div');
     row.classList.add('row');
     for(let j = 0; j < 16; ++ j){
         const square = document.createElement('div');
         square.classList.add("square");
+        square.id = "square" + squareID;
+        squareID++;
+        square.addEventListener("mouseover", () => {squareHover(square);});
         row.appendChild(square);
     }
     grid.appendChild(row);
+}
+
+function squareHover(square){
+    square.classList.add("hover");
 }
